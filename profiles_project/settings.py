94fed4003d700a05ffc4bd609c3aafbb6922cf2d
Rewardz <ankit._seeking_job@yahoo.co.in>
2019-07-19 11:15:33 +0530

    updated allowed hosts

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'dfsq!(*iv_r@myi_$h(hi*sun84p#2qozl#x+y&a@b3=(o9al^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-54-245-190-196.us-west-2.compute.amazonaws.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
